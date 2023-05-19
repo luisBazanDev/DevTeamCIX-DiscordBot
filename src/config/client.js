ddc4fd3ee422206be33fd99146f85cd12a7116c9
Luis Bazan <luis@bazan.pe>
2023-05-19 12:24:21 -0500

    Add social config

--- src/config/client.js
+++ src/config/client.js
@@ -18,4 +18,26 @@ export default {
       value: "c++",
     },
   ],
+  social: [
+    {
+      urls: ["https://github.com/"],
+      name: "GitHub",
+      valued: "github",
+    },
+    {
+      urls: ["https://www.instagram.com/"],
+      name: "Instagram",
+      valued: "instagram",
+    },
+    {
+      urls: ["https://www.facebook.com/"],
+      name: "Facebook",
+      valued: "facebook",
+    },
+    {
+      urls: ["https://twitch.tv/"],
+      name: "Twitch",
+      valued: "twitch",
+    },
+  ],
 };
