a9af401d9ff1b5542d9d97cd9198519d802d7527
Luis Bazan <luis@bazan.pe>
2023-06-17 21:26:25 -0500

    test for deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import { ShardingManager } from "discord.js";
 
+// test for update on hosting
 const manager = new ShardingManager("./src/bot.js", {
   totalShards: "auto",
   token: process.env.DISCORD_TOKEN,
